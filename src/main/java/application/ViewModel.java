5d188ab5bc7f8f89eadf4a25c3b100731bb3dde3
DEVO27 <A.G-Zoe@outlook.com>
2022-11-13 15:14:57 -0600

    Updated View And ScoreBoard.fxml

--- src/main/java/application/ViewModel.java
+++ src/main/java/application/ViewModel.java
@@ -1,34 +1,40 @@
 package application;
 
+import application.model.Scoreboard;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * JavaFX App
  */
-public class App extends Application {
+public class ViewModel extends Application implements Initializable {
 
     private static Scene scene;
+    private final Scoreboard scoreBoard = new Scoreboard();
+    @FXML
+    private ListView<String> myListView;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(new FXMLLoader(ViewModel.class.getResource("Scoreboard.fxml")).load());
+        stage.setTitle("ScoreBoard");
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        myListView.getItems().addAll(scoreBoard.ToString());
     }
 
     public static void main(String[] args) {
