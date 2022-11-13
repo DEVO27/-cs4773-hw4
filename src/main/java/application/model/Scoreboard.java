0ff5ce051f19203492b0faa99950682c356746c6
DEVO27 <A.G-Zoe@outlook.com>
2022-11-13 12:18:46 -0600

    Added Classes Changes

---
+++ src/main/java/application/model/Scoreboard.java
@@ -0,0 +1,40 @@
+package application.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scoreboard {
+    private List<Team> teams = new ArrayList<>();
+
+    public String[] ToString() {
+        String[] strings = new String[teams.size()];
+        int index = 0;
+        for (Team team : teams) {
+            strings[index++] = String.valueOf(team);
+        }
+        return strings;
+    }
+
+    private void generateTeams() {
+        for (int i = 0; i < 5; i++) {
+            teams.add(new Team());
+        }
+    }
+
+    public Team getTeam(int index) throws IndexOutOfBoundsException {
+        return getTeam(index);
+    }
+
+    public List<Team> getTeams() {
+        return teams;
+    }
+
+
+    public Scoreboard() {
+        generateTeams();
+    }
+
+    public void setTeams(List<Team> teams) {
+        this.teams = teams;
+    }
+}
